5a451f2816133bdbdbf1ba848d78f404c300ff3a
Tong Chia <tongchia@live.com>
2018-03-20 16:18:22 +0800

    fix validateSync; remove normalizes;

--- src/Schema.js
+++ src/Schema.js
@@ -1,112 +1,101 @@
-import {
-  assign, keys, simpleType, template, mapValues,
-  isArray, isString, toPromise
-} from './utils';
+import {assign, keys, simpleType, mapValues, includes, toSync,
+  isObject, isArray, isUndefined, isFunction} from './utils';
 import {simpleTypeDic as types} from './TypeDictionary';
 import {validates} from "./ValidateMap";
-import {Any} from "./types";
+import {Any, ValidationError} from "./types";
 
 const schemas = new Map();
 
-const normalizes = {
-  Object: (define) => types.has(define.type) ?
-    {...define} :
-    {type: Object, properties: {...define}},
-  Array : (items) =>
-    (items.length === 0) ?
-      {type: Array, items: {type: Any}} :
-    (items.length === 1) ?
-      {type: Array, items: items[0]} :
-    (types.has(items)) ? //TODO: 且不重复
-      {type: items} :
-      {type: Array, items},
-  $: (define) => (types.has(define) && {type: define}),
-  get String() {return this.$},
-  get Function() {return this.$}
-};
-
 const normalize = (define) => {
-  let normalizer = normalizes[simpleType(define)];
-  return normalizer && normalizer(define);
+
+  if (isObject(define))
+    return (isArray(define.type) ?
+      (define.type.length > 0 && define.type.every(types.has)) :
+      (types.has(define.type))) ?
+        {...define} :
+        {type: Object, properties: {...define}};
+
+  if (isArray(define))
+    return (define.length === 0) ?
+      {type: Array, items: {type: Any}} :
+      (define.length === 1) ?
+        {type: Array, items: normalize(define[0])} :
+        (define.every(t => types.has(t))) ?
+          {type: [...define]} :
+          {type: Array, items: [...define]};
+
+  if (types.has(define))
+    return {type: define};
+
 };
 
-const initProps = (props) => mapValues(props, Schema);
-const initItems = (items) => isArray(items) ? items.map(Schema) : Schema(items);
-
-function Schema ($id, define, options) {
+//TODO: Add $title;
+function Schema (define, options) {
   // recursive
-  if (!new.target) return new Schema($id, define, options);
-
-  if (!isString($id) || types.has($id)) define = $id; options = define;
+  if (!new.target) return new Schema(define, options);
 
   let formatted = normalize(define);
   if (!formatted) throw new TypeError('Invalid argument');
 
   this.assign(formatted);
 
-  if (options.freeze) Object.freeze(this);
-  if ($id) schemas.set($id, this);
+  if (options && options.freeze) Object.freeze(this);
 }
 
-Schema.prototype.set = function (prop, value) {
-  if (prop === 'type')
-    this.type = types.get(value);
-  else if (prop === 'items')
-    this.items = initItems(value);
-  else if (prop === 'properties')
-    this.properties = initProps(value);
-  else
-    this[prop] = value;
+const proto = Schema.prototype;
+
+proto.set_type = function (type) {
+  this.type = types.get(type);
 };
 
-Schema.prototype.assign = function (define) {
+proto.set_items = function (items) {
+  this.items = isArray(items) ? items.map(Schema) : Schema(items);
+};
+
+proto.set_properties = function (props) {
+  this.properties = mapValues(props, Schema);
+};
+
+proto.set = function (attribute, value) {
+  if (includes(['type', 'items', 'properties'], attribute))
+    this['set_' + attribute](value);
+  else
+    this[attribute] = value;
+};
+
+proto.assign = function merge (define) {
   //TODO: Schema.schema, check define. (like json-schema)
-  // if (!(define instanceof Schema) || defineSchema.validate(define))
-  //   throw new Error('not validated');
   keys(define).forEach((prop) => {this.set(prop, define[prop])})
 };
 
-Schema.prototype.validateSync = function validateSync (value, force) {
-  let _validates = this.validates;
-  for (const valid in _validates) {
-    if (Object.prototype.hasOwnProperty.call(_validates, valid)) {
-      let {async, validator} = _validates[valid];
-      if ((!force && !async) && !validator(value, this[valid]))
-        return false;
-    }
-  }
-  return true;
+proto.validateSync = function validateSync (value, force) {
+  if (force) return toSync(this.validate)(value);
+
+  let valueType = validates.get('__type').validator(value, this);
+  if (!valueType) return false;
+
+  return keys(this)
+    .map(attribute => validates.get(attribute, valueType))
+    .filter((s) => !(isUndefined(s) || s.async || s.await))
+    .every(({validator}) => validator(value, this));
 };
 
-Schema.prototype.validate = function validate (value, callback) {
-  if (!callback) return this.validateSync(value);
-  let _validates = this.validates, errors = [];
+proto.validate = function validate (value, callback) {
+  if (!isFunction(callback)) return this.validateSync(value, callback);
 
-  // TODO: should return one Error include errors. (mongoose)
-  Promise.all(keys(_validates).forEach(valid => async () => {
-    let {async, validator, message, error = Error} = _validates[valid];
-    let _validator = async ? toPromise(validator) : validator;
-    let result = await _validator(value, this[valid]);
-    if (!result)
-      return new error(template(message, {value, valid}))
-  })).then(result => callback(undefined, result)).catch(callback)
+  // let valueType = validates.get('__type').validator(value, this);
+  // if (isUndefined(type)) return callback(new Error('value is undefined'));
+  //TODO: async validate;
 };
 
-Schema.prototype.toJSON = function (version = 'v4') {
+proto.toJSON = function toJSON (version) {
   let type = this.type;
   return assign({}, this, {type: types.name(type)})
 };
 
-Schema.prototype.inspect = function inspect () {
+proto.inspect = function inspect () {
   return `Schema {type: ${types.name(this.type)}}`
 };
-
-Object.defineProperty(Schema.prototype, 'validates', {
-  enumerable: true,
-  get: function getValidates () {
-    return mapValues(this, (v, k) => validates.get(k))
-  }
-});
 
 Schema.Types = types;
 Schema.validates = validates;
