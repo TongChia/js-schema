d4412c88bfb6c2f8c46d715f676958383eede6f9
Tong Chia <tongchia@live.com>
2018-03-27 17:04:31 +0800

    add `async properties` validate;

--- src/ValidateMap.js
+++ src/ValidateMap.js
@@ -1,7 +1,7 @@
 import {
-  isUndefined, isBoolean, isDate, isString, isSymbol, isRegExp, isObject,
-  isArray, isFunction, isInteger, isNumber, isAsyncFunction, isConstructor,
-  isInvalidString, assign, keys, contains, toArray
+  isUndefined, isBoolean, isString, isSymbol, isRegExp, isObject,
+  isArray, isFunction, isInteger, isAsyncFunction, isConstructor,
+  isInvalidString, assign, keys, contains, toArray, different, template, isError
 } from "./utils";
 import {Any, ValidationError} from './types';
 
@@ -11,76 +11,74 @@ export default class ValidateMap extends Map {
 
   /**
    * Set `validate`.
-   * @param {(String|*)} prop
+   * @param {String} prop
    * @param {(Object|Function)} validate
-   * @param {(Array|*)} [scopes]
-   * @param {Boolean} [isAsync]
+   * @param [scope]
    * @return {ValidateMap}
    */
-  set(prop, validate, scopes, isAsync) {
-    let conf = {async: false, message: '${value} is not validated'};
+  set(prop, validate, scope) {
+    let scopes = toArray(scope)
+      , {validator = validate, message = '${value} is not validated', error = ValidationError} = validate
+      , _async = validate.async
+      , _await = validate.await;
 
-    if (isConstructor(scopes)) scopes = [scopes];
-    if (isArray(scopes) && scopes.every(isConstructor)) this.scopes.set(prop, scopes);
-
-    if (isConstructor(prop))
-      conf.specified = prop;
-    else if (isInvalidString(prop))
+    if (isInvalidString(prop))
       throw new TypeError(`Invalid argument (${prop})`);
+    if (!isFunction(validator))
+      throw new TypeError(`Invalid argument (${validator})`);
 
-    if (isFunction(validate))
-      validate = {validator: validate};
-    else if (!isObject(validate))
-      throw new TypeError(`Invalid argument (${validate})`);
-    else if (!isFunction(validate.validator))
-      throw new TypeError(`Invalid argument (${validate.validator})`);
+    if (isAsyncFunction(validator)) _await = true;
+    if (/^async /.test(prop)) _async = !_await;
+    else if (_async || _await) prop = 'async ' + prop;
 
-    if (isAsyncFunction(validate.validator)) conf.await = true;
+    super.set(prop, {async: _async, await: _await, validator, message, error});
 
-    super.set(prop, assign(conf, validate));
+    if (scopes.length && scopes.every(isConstructor))
+      this.scopes.set(prop, scopes);
 
     return this;
   }
 
-  get(attribute, type) {
-    if (!this.scopes.has(attribute) || this.scopes.get(attribute).includes(type))
-      return super.get(attribute);
+  has(attribute, scope) {
+    return (!this.scopes.has(attribute) || this.scopes.get(attribute).includes(scope)) && super.has(attribute)
   }
 }
 
 export const validates = new ValidateMap()
-  .set(String, isString)
-  .set(Number, isNumber)
-  .set(Boolean, isBoolean)
-  .set(Date, isDate)
-  .set(Object, isObject)
-  .set(Array, isArray)
-  .set(Symbol, isSymbol)
-  .set(RegExp, isRegExp)
-  .set(Any, (data) => !isUndefined(data))
   .set('enum', (data, schema) => schema.enum.includes(data))
   .set('min', (data, {min}) => (data >= min), Number)
   .set('max', (data, {max}) => (data <= max), Number)
   .set('match', (data, {match}) => match.test(data), String)
-  .set('minLength', (data, {min}) => (data.length > min), String)
-  .set('maxLength', (data, {max}) => (data.length < max), String)
-  .set('minItems', (data, {min}) => (data.length > min), Array)
-  .set('maxItems', (data, {max}) => (data.length < max), Array)
+  .set('minLength', (data, {min}) => (data.length >= min), String)
+  .set('maxLength', (data, {max}) => (data.length <= max), String)
+  .set('minItems', (data, {min}) => (data.length >= min), Array)
+  .set('maxItems', (data, {max}) => (data.length <= max), Array)
   .set('integer', (data, {integer}) => (!integer || isInteger(data)), Number)
   .set('uniq', (data, {uniq}) => (!uniq || data.every((v, i, d) => d.indexOf(v) === i)), Array)
   .set('before', (data, {before}) => (new Date(data) < new Date(before)), Date)
   .set('after', (data, {after}) => (new Date(data) > new Date(after)), Date)
-
-  // check type first.
-  .set('__type', (data, {type}) =>
-    toArray(type).find(t =>
-      validates.has(t) ? validates.get(t).validator(data) : (data instanceof t)
-    )
-  )
   // .set('required', (data, {required}) => (!required && isUndefined(data)))
-  .set('$properties', () => {
-    // TODO: check properties with callback
-  })
+  .set('properties', async (data, {properties}) => {
+    let props = keys(properties);
+    let _keys = keys(data);
+    let errors = {};
+    let warnings = [];
+
+    let extra = different(props, _keys);
+
+    if (extra.length) return new ValidationError('extra' + extra);
+
+    for (let prop of props) {
+      let _schema = properties[prop];
+      let _data = data[prop];
+      if (isUndefined(_data) && !_schema.required) return true;
+      let result = await _schema.validateWait(_data);
+      if (!result) throw new ValidationError(template('123', {prop}));
+      if (isError(result)) throw result;
+    }
+
+    return true;
+  }, Object)
   .set('properties', (data, {properties}) => {
     let _props = keys(properties);
     let _keys = keys(data);
