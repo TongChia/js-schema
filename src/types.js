a92b3f470cc59c438cb71b4273ea901a590c462c
Tong Chia <tongchia@live.com>
2018-03-27 17:01:57 +0800

    optimize `ValidationError`;

--- src/types.js
+++ src/types.js
@@ -10,12 +10,22 @@ Any.prototype.toJSON = function () {
   return this.value;
 };
 
-export class ValidationError extends Error {
+// export class ValidationError extends Error {
+//
+//   constructor(message, errors) {
+//     super(message);
+//     this.errors = errors;
+//   }
+// }
 
-  constructor(message, errors) {
-    super(message);
-    this.errors = errors;
-  }
+export function ValidationError (message, errors) {
+  if (!new.target) return new ValidationError(message, errors);
+  this.constructor.prototype.__proto__ = Error.prototype;
+  Error.call(this);
+  this.message = message;
+  if (errors) this.errors = errors;
+  if (Error.captureStackTrace) Error.captureStackTrace(this, this.constructor);
+  else this.stack = (new Error).stack;
 }
 
 ValidationError.prototype.name = 'ValidationError';
